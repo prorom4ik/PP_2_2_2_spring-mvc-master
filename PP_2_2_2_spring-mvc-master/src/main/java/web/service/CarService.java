c89f0290d11a0152a63fa4566154e8448c752c12
prorom4ik <m2009309@edu.misis.ru>
2022-03-14 12:04:04 +0300

    Commit

--- PP_2_2_2_spring-mvc-master/src/main/java/web/service/CarService.java
+++ PP_2_2_2_spring-mvc-master/src/main/java/web/service/CarService.java
@@ -1,12 +1,22 @@
 package web.service;
 
-import web.controller.CarController;
+import org.springframework.stereotype.Service;
 import web.model.Car;
-
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarService {
+
+    private static List<Car> cars = new ArrayList<Car>() {{
+        add(new Car("S",5,"China"));
+        add(new Car("A",4,"Russia"));
+        add(new Car("C",2,"Japan"));
+        add(new Car("Y",1,"USA"));
+        add(new Car("Z",6,"Ukraine"));
+    }};
+
     public static List<Car> getNCars(int number) {
-        return CarController.getCars().stream().limit(number).toList();
+        return cars.stream().limit(number).toList();
     }
 }
